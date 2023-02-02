934fc381adcea5679f1fad4d4f89a247b64bcccf
Hiram Silva <111798116+Bubby896@users.noreply.github.com>
2023-02-02 09:03:42 -0800

    Create student.java

---
+++ student.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Scanner;
+
+public class student {
+    
+    public static void main(String args[])
+    throws IOException{
+        
+        String name = "Name";
+        String year = "Year";
+        String school = "School";
+
+        Path txtFile = Path.of("/Users/Hiram Silva/Downloads/student.txt");
+
+        Scanner Answer = new Scanner(System.in);
+
+        System.out.println("What is your name?\n");
+
+        if(Answer.hasNextLine() == true) name = Answer.nextLine();
+
+        Files.writeString(txtFile, "\nHello " + name + "!");
+
+
+        System.out.println("\nWhat year are you in high school? (ie: Freshmen, Sophomore...)\n");
+
+        if(Answer.hasNextLine() == true) year = Answer.nextLine();
+
+        Files.writeString(txtFile, "\nYou are a " + year + ", and you go to ", StandardOpenOption.APPEND);
+
+
+        System.out.println("\nWhat school do you go to?\n");
+
+        if(Answer.hasNextLine() == true) school = Answer.nextLine();
+
+        Files.writeString(txtFile, school + ".", StandardOpenOption.APPEND);
+
+
+        String file_content = Files.readString(txtFile);
+
+        System.out.println(file_content);
+    }
+}
