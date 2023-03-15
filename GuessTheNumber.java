03ad76eb2e00073c15ed92a75d57fabacded748d
Hiram Silva <111798116+Bubby896@users.noreply.github.com>
2023-03-15 12:04:39 -0700

    Update GuessTheNumber.java

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -12,9 +12,13 @@ public class GuessTheNumber {
         int RandomNumber = 0;  //Creates an int for the number that will be chosen randomly
         String User1Guess = "0";  //Creates a string to be used for the first User's guess
         String User2Guess = "0";  //Creates a string to be used for the seconds User's guess
+        int User1GuessNumber = 0;  //Creates an int to be used for the numerical value of User 1's guess
+        int User2GuessNumber = 0;  //Creates an int to be used for the numerical value of User 2's guess
         Scanner UserNames = new Scanner(System.in);  //Creates a scanner to read keyboard input for the user names
         Scanner UserGuesses = new Scanner(System.in);  //Creates a scabber to read keyboard input for the user guesses
         Random Random = new Random(); //Creates a "Random" for getting a random number
+        boolean User1AnswerValid = false;  //Creates a boolean to allow the try catch block to run later in the code
+        boolean User2AnswerValid = false;  //Creates a boolean to allow the try catch block to run later in the code
 
         System.out.println("\nPlease input a name for User 1: \n"); 
 
@@ -32,13 +36,51 @@ public class GuessTheNumber {
 
         TimeUnit.SECONDS.sleep(1);  //Pauses the output for 1 second
 
-        System.out.println(User1 + "'s Guess: \n");  //Asks for User 1's guess
+        while (!User1AnswerValid){  //Tells the program to execute the following code if User1AnswerValid is false
 
-        if(UserGuesses.hasNextLine() == true) User1Guess = UserGuesses.nextLine();  //Reads the keyboard input for User 1's guess
+        try {  //Checks for errors in the user's input
 
-        System.out.println("\n \n" + User2 + "'s Guess: \n");  //Asks for User 2's guess
+          System.out.println(User1 + "'s Guess: \n");  //Asks for User 1's guess
 
-        if(UserGuesses.hasNextLine() == true) User2Guess = UserGuesses.nextLine();  //Reads the keyboard input for User 2's guess
+          if(UserGuesses.hasNextLine() == true) User1Guess = UserGuesses.nextLine();  //Reads the keyboard input for User 1's guess
+  
+          User1GuessNumber = Integer.valueOf(User1Guess);  //Sets User1GuessNumber to the numerical value of the string User1Guess
+
+          User1AnswerValid = true;  //Causes the program to not loop this code
+  
+        } catch (Exception e){  //This tells the program what to do if the user inputs an invalid value
+
+          TimeUnit.SECONDS.sleep(1);  //Pauses the output for 1 second
+
+          System.out.println("\nPlease input a valid guess\n");  //Asks the user to enter a different input
+
+          TimeUnit.SECONDS.sleep(1);  //Pauses the output for 1 second
+        }
+
+      }
+
+      while (!User2AnswerValid){  //Tells the program to execute the following code if User2AnswerValid is false
+
+        try {  //Checks for errors in the user's input
+
+          System.out.println("\n" + User2 + "'s Guess: \n");  //Asks for User 2's guess
+
+          if(UserGuesses.hasNextLine() == true) User2Guess = UserGuesses.nextLine();  //Reads the keyboard input for User 2's guess
+  
+          User2GuessNumber = Integer.valueOf(User2Guess);  //Sets User2GuessNumber to the numerical value of the string User2Guess
+
+          User2AnswerValid = true;  //Causes the program to not loop this code
+  
+        } catch (Exception e){  //This tells the program what to do if the user inputs an invalid value
+
+          TimeUnit.SECONDS.sleep(1);  //Pauses the output for 1 second
+
+          System.out.println("\nPlease input a valid guess\n");  //Asks the user to enter a different input
+
+          TimeUnit.SECONDS.sleep(1);  //Pauses the output for 1 second
+        }
+
+      }
 
         TimeUnit.SECONDS.sleep(1);  //Pauses the output for 1 second
 
@@ -54,7 +96,7 @@ public class GuessTheNumber {
 
         TimeUnit.SECONDS.sleep(1);  //Pauses the output for 1 second
 
-        if ((Integer.valueOf(User1Guess) - RandomNumber) <= (Integer.valueOf(User2Guess) - RandomNumber)) {  //Finds which guess is closer
+        if (Math.abs(User1GuessNumber - RandomNumber) <= Math.abs(User2GuessNumber - RandomNumber)) {  //Finds which guess is closer
 
             System.out.println("\n" + User1 + " Wins!");  //Prints the winner
 
