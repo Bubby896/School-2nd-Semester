9b8e8387048bb1f5a5e271f36bfd95b03b05cda1
Hiram Silva <111798116+Bubby896@users.noreply.github.com>
2023-04-11 16:34:05 -0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,42 @@
+/*
+Error filled file created by
+Kimg@techtrepacademy.com
+for my java class to learn troubleshooting on
+*/
+public class Main {
+    public static void main(String args[]){
+
+    System.out.println("Hello everybody!");
+    System.out.println("Am I doing this correctly?\n");
+    for (int i=0;i<10;i++){
+        System.out.println("The number i is " + i);
+    }
+
+    for (int i=0; i < 10; i++){
+        System.out.println("\nThe new number i is " + i);
+
+        if (i == 0){
+            System.out.println("i is equal to zero");
+    
+        }else {
+            System.out.println("i is not equal to zero");
+    
+        }
+    }
+
+    //what other errors can I put into a source code file?
+
+}
+}
+    //Changes:
+    //Changed the name of the file to match the class
+    //Added a "public static void main(String args[]) {} so that the code could run"
+    //Made the "For" in one of the loops lowercase
+    //Replaced a colon in a loop with a semicolon
+    //Replaced an 'm' with an 'n' for a print statement
+    //Capitalized 'system' in a print statement
+    //Moved the if else statement into the for loop so it could use the i variable
+    //Switched the direction of the greater than sign for the for loop so it works
+    //Changed some print statements to println for read-ability
+    //Added some gaps to the output for read-ability
+    //Small grammatical :)
