e099ba5d631121f9f789261bf613ac9ed6b589b6
Hiram Silva <111798116+Bubby896@users.noreply.github.com>
2023-04-11 16:32:41 -0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        System.out.println("this program has an error or two in it that should be simple to find");
+        System.out.println("I'm really good at making mistakes when programing");
+        
+
+    }
+}
+//For this project I replaced the colon on line 5 with a semicolon, and added a semicolon to line 4
